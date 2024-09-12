a7f2d8ddd5d1f8419e96bd1b2c4828f64849be74
Chimtamreddy <kr7348202@gmail.com>
2024-09-12 07:12:01 +0530

    jenkins

--- main.tf
+++ main.tf
@@ -3,5 +3,6 @@ resource "aws_ssm_parameter" "main" {
   name  = each.key
   type  = each.value["type"]
   value = each.value["value"]
+  key_id = "arn:aws:kms:us-east-1:014498634764:key/1d8a17d0-166d-43e5-b9e9-d38771f90b2d"
 }
 
